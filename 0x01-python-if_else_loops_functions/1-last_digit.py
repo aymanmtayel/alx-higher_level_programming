2b976568536b6c2622dd4fccd08e408dc78e0ede
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-02 22:37:05 +0200

    task01 fix the variables name

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,5 @@ elif (number > 0 and 0 < l < 6):
     print(f"Last digit of {number} is {l} and is less than 6 and not 0")
 elif (number < 0 and l > 0):
     print(f"Last digit of {number} is {-1 * l} and is less than 6 and not 0")
-elif (last == 0):
+elif (l == 0):
     print(f"Last digit of {number} is {l} and is 0")

ba78e2230fa4a5f05be8268beae949970800cf25
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-03 00:59:51 +0200

    fix length

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+def islower(c):
+    asci = ord(c)
+    if 96 < asci < 123:
+        return True
+    else:
+        return False
+
+
 def uppercase(str):
-    length = len(str) - 1
-    for i in range(len(str)):
-        if 96 < ord(str[i]) < 123:
-            print("{}".format(chr(ord(str[i]) - 32)), end="")
-        else:
-            print("{}".format(str[i]), end="")
-    print("{}".format(""))
+    for char in str:
+        if char is islower:
+            char = chr(ord(i) - 32)
+        print("{}".format(char), end="")
+    print("")

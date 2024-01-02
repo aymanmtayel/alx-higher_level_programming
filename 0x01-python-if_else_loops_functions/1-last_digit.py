c1941bbfb9a47837347c6607277c48a173707b79
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-02 22:35:55 +0200

    task01 fix length of the line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
-if (number > 0 and last > 5):
-    print(f"Last digit of {number} is {last} and is greater than 5")
-elif (number > 0 and 0 < last < 6):
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
-elif (number < 0 and last > 0):
-    print(f"Last digit of {number} is {-1 * last} and is less than 6 and not 0")
+l = abs(number) % 10
+if (number > 0 and l > 5):
+    print(f"Last digit of {number} is {l} and is greater than 5")
+elif (number > 0 and 0 < l < 6):
+    print(f"Last digit of {number} is {l} and is less than 6 and not 0")
+elif (number < 0 and l > 0):
+    print(f"Last digit of {number} is {-1 * l} and is less than 6 and not 0")
 elif (last == 0):
-    print(f"Last digit of {number} is {last} and is 0")
+    print(f"Last digit of {number} is {l} and is 0")

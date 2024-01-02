556a0ebcc0c0df63da125a10882a41b656629621
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-03 00:14:54 +0200

    task08 use only .format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     length = len(str) - 1
     for i in range(len(str)):
         if 96 < ord(str[i]) < 122:
-            print(f"{chr(ord(str[i]) - 32)}", end="" if i < length else "\n")
+            print("{}".format(chr(ord(str[i]) - 32)), end="" if i < length else "\n")
         else:
-            print(str[i], end="")
+            print("{}".format(str[i]), end="")

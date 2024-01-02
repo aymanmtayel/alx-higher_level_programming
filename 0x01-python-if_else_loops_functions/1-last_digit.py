636fe149a7982b690494e24b421fc6db5d8b7951
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-02 22:30:19 +0200

    task01 fix first letter

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 last = number % 10
 if (number > 0 and last > 5):
-    print(f"last digit of {number} is {last} and is greater than 5")
+    print(f"Last digit of {number} is {last} and is greater than 5")
 elif (number > 0 and 0 < last < 6):
-    print(f"last digit of {number} is {last} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 elif (number < 0 and last > 0):
-    print(f"last digit of {number} is {last} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 elif (last == 0):
-    print(f"last digit of {number} is {last} and is 0")
+    print(f"Last digit of {number} is {last} and is 0")

e1db422de4b0e2df7294d721b862e5fc4e82e7a9
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-04 22:26:25 +0200

    task02 remove i def

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,11 @@ if __name__ == "__main__":
     import sys
     values = sys.argv[0:]
     length = len(sys.argv) - 1
-    i = 1
     if length == 0:
         print("0 arguments.")
     elif length == 1:
         print("1 argument:\n1: {}".format(values[0]))
     else:
         print("{} arguments:".format(length))
-        for i in range(1, length + 1):
-            print("{}: {}".format(i, values[i]))
+        for i in range(length):
+            print("{}: {}".format(i + 1, values[i + 1]))

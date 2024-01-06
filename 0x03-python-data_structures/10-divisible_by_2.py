84d7cea546c7455fe583a039603c678fc5f19af4
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-06 13:48:28 +0200

    task10 fix indentation

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@ def divisible_by_2(my_list=[]):
     result = [0]
     length = len(my_list)
     for k in range(length - 1):
-            if my_list[k] % 2 == 0:
-                result.append(True)
-            else:
-                result.append(False)
+        if my_list[k] % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
     return result

8faa95816d76103a2790fa4d10f5c57d975589a3
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-06 13:51:04 +0200

    task10 add None condition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = [0]
+    result = []
     length = len(my_list)
-    for k in range(length - 1):
-        if my_list[k] % 2 == 0:
-            result.append(True)
-        else:
-            result.append(False)
+    if my_list is None:
+        return None
+    else:
+        for k in range(length - 1):
+            if my_list[k] % 2 == 0:
+                result.append(True)
+            else:
+                result.append(False)
     return result

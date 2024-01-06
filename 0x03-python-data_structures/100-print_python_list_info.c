7969e574980c1d18c72de53df7aeb5a1cb06525d
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-06 16:23:58 +0200

    task100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <python.h>
+
+/**
+ * print_python_list_info - function that prints some basic
+ * info about Python lists.
+ * @p: a python list to be evaluated
+ * Return: Void
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int length, size_alloc, counter;
+	pyobject *object_t;
+
+	length = Py_SIZE(p);
+	size_alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] size of the python list = %d\n", length);
+	printf("[*] Allocated = %d\n", size_alloc);
+
+	for (counter = 0; counter < length; counter++)
+	{
+		printf("Element %d: ", counter);
+		object_t = PyList_GetItem(p, counter);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}

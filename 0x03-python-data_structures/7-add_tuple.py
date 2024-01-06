170e00bf7f988b44fe1ffebfd0800642fbf02cfa
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-06 12:02:24 +0200

    task07

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/use/bin/pyhone
+def add_tuple(tuple_a=(), tuple_b=()):
+    ae1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    ae2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    be1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    be2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (ae1 + be1, ae2 + be2)

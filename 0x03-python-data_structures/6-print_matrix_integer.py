d28b9340f266df1d6e8d53f634096bde88e4d8fd
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-06 11:46:27 +0200

    task06 fix no matrix return

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for i in matrix:
-        for k, num in enumerate(i):
-            if k == len(i) - 1:
-                print("{:d}".format(num), end='\n')
-            else:
-                print("{:d}".format(num), end=" ")
+    else:
+        for i in matrix:
+            if len(i) == 0:
+                print()
+            for k, num in enumerate(i):
+                if k == len(i) - 1:
+                    print("{:d}".format(num), end='\n')
+                else:
+                    print("{:d}".format(num), end=" ")

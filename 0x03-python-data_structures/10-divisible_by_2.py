bf9ab230b6ec7e970fde5118ded5a6dddb2df8e3
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-06 13:52:58 +0200

    task10 fix condtions

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    result = []
-    length = len(my_list)
     if my_list is None:
         return None
     else:
-        for k in range(length - 1):
-            if my_list[k] % 2 == 0:
+        result = []
+        for k in my_list:
+            if k % 2 == 0:
                 result.append(True)
             else:
                 result.append(False)

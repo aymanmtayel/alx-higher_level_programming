764eaa25eeb5d112ae1a9bf850bd7ccbc9bc6af2
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-10 23:08:07 +0200

    task00 remove for loop

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squared_matrix = []
-    for row in matrix:
-        square_row = [x ** 2 for x in row]
-        squared_matrix.append(square_row)
-    return squared_matrix
+    return list(map(lambda squared: list(map(lambda x: x**2, squared)), matrix))

5d0007178208c60b58521f07569c630d0719cf04
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-23 22:55:37 +0200

    Task01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """Define a class named Square"""
+
+    def __init__(self, size):
+        """initiator.
+
+        Args:
+            size: length of any side of the square.
+        """
+    self.__size = size

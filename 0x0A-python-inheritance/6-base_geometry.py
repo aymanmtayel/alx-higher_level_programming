c596cf7142d64228fe3c1e04d99bd31f51d4e7c2
Ayman Tayel <eng.aymantayel@gmail.com>
2024-02-05 21:29:20 +0200

    Task06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """ a class BaseGeometry"""
+    def area(self):
+        """function to calculate the area (not implemented yet)"""
+        raise Exception("area() is not implemented")

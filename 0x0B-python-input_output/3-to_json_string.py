f742158865a74320ad89dc2069a3df80c5ebf784
Ayman Tayel <eng.aymantayel@gmail.com>
2024-02-06 20:43:37 +0200

    task03

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""JSON representation module"""
+import json
+
+def to_json_string(my_obj):
+    """function to return the JSON representation of an object
+
+    Args:
+        my_obj: the object to be converted
+
+    Returns: the JSON representation
+    """
+    return json.dumps(my_obj)

636fc805a613c224b8b295e418de9c81a0d3f3e4
Ayman Tayel <eng.aymantayel@gmail.com>
2024-03-12 23:14:33 +0200

    Task09

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
--- creates a table in my database 
+-- creates a second table in my database and the database is argument
+-- usage mysql -hlocalhost -uuser -p <database>
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "JOHN", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);

55f4a4d570e306633a777599708577237f88965e
Ayman Tayel <eng.aymantayel@gmail.com>
2024-03-14 00:08:39 +0200

    Task100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,8 @@
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT name FROM tv_genres
+WHERE name NOT IN (SELECT a.name FROM tv_genres a 
+	LEFT JOIN tv_show_genres b 
+	ON a.id = b.genre_id
+	LEFT JOIN tv_shows c
+	ON b.show_id = c.id WHERE c.title = 'Dexter')
+ORDER BY name ASC;

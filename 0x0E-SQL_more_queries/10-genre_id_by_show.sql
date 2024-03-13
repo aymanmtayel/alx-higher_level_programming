283893fa4783c7b44b8a8ca9d85e476b3d46f229
Ayman Tayel <eng.aymantayel@gmail.com>
2024-03-13 22:51:32 +0200

    fix where statment

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked.
-SELECT shows.title, genres.show_id
-FROM tv_show_genres genres LEFT JOIN tv_shows shows
-ON genres.show_id = shows.id
-ORDER BY shows.title, genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

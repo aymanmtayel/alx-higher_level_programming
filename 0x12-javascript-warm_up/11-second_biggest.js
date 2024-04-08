6b7ea9430d34fa7f2a382a9634c2a75b0dbe885a
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 01:59:04 +0200

    Task11 another approach

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,4 +4,4 @@ const sorted = process.argv.sort();
 
 if (process.argv.length <= 3) {
   console.log(0);
-} else { console.log(sorted[sorted.length - 2]); }
+} else { console.log(sorted.reverse()[1]); }

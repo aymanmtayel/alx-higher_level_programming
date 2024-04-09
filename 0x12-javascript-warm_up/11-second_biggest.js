260a0feeade2b67facc0c943981e839807e44c8a
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 02:18:47 +0200

    Task11 final fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const list = process.argv.slice(2, process.argv.length).map(Number)
+const list = process.argv.slice(2, process.argv.length).map(Number);
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const sorted = list.sort((a, b)=> a - b);
+  const sorted = list.sort((a, b) => a - b);
   console.log(sorted[sorted.length - 2]);
 }

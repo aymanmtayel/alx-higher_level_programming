a83ffacf29ebc6439310cab8691c14c3f0fd730d
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 02:17:27 +0200

    Task11 fix arrow function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const list = process.argv.slice(2, process.argv.length).map(Number)
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const sorted = list.sort(a, b)=> a - b;
+  const sorted = list.sort((a, b)=> a - b);
   console.log(sorted[sorted.length - 2]);
 }

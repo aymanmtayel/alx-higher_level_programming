6f29399b7e9b7cdfa644e84f336514edc21ff6ea
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 02:07:34 +0200

    Task11 fix no strings

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
-const sorted = process.argv.sort();
+const list = process.argv.slice(2).map(Number);
 
 if (process.argv.length <= 3) {
   console.log(0);
-} else { console.log(sorted.reverse()[1]); }
+} else {
+  const sorted = list.sort();
+  console.log(sorted.reverse()[1]);
+}

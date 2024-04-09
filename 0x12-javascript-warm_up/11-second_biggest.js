7328200cc3a1c65ec852d471fa5aa516c0de1bfd
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 02:15:59 +0200

    Task11 no need to handle strings

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const list = process.argv.slice(2).map(Number).filter(value => !isNaN(value));
+const list = process.argv.slice(2, process.argv.length).map(Number)
 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const sorted = list.sort();
-  console.log(sorted.reverse()[1]);
+  const sorted = list.sort(a, b)=> a - b;
+  console.log(sorted[sorted.length - 2]);
 }

6ce296130a7a14a142def23080525666af339a32
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 13:08:48 +0200

    task check if empty

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let i; const temp = [0];
+  if (list.length === 0) { return []; }
+
+  let i;
+  const temp = [];
   for (i = 0; i < list.length; i++) {
     temp[i] = list[list.length - 1 - i];
   }

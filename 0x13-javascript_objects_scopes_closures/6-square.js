81e484573fd95043575d66bdcce73c2e5af23abb
Ayman Tayel <eng.aymantayel@gmail.com>
2024-04-09 12:40:35 +0200

    Task 06 remove inst

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,17 +3,12 @@
 const old = require('./5-square');
 
 module.exports = class Square extends old {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c) {
     let i;
     if (c === undefined) {
       this.print();
     } else {
-      for (i = 0; i < this.size; i++) { console.log(c.repeat(this.size)); }
+      for (i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
 };

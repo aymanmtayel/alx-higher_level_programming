2f1ade522e9ee7407d889ddb6b17c235afcb70e0
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-25 23:47:43 +0200

    Task102 put your server

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #cript that makes a request to 0.0.0.0:5000/catch_me and get got me
-curl -sLX PUT http://3eb037533698.e1c9ecb0.alx-cod.online:5000/catch_me -d "user_id=98" -H "Origin:School"
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:School"

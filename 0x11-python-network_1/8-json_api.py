03bacd46c2f8146d17b1bdef4e52d8f7cae96d3e
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-26 22:28:13 +0300

    task08

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    UR = "http://0.0.0.0:5000/search_user"
+    URL = "http://160996c252bf.813da4bf.alx-cod.online:5000/search_user"
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    query = {"q": q}
+    response = requests.post(URL, data=query)
+    try:
+        jsonr = response.json()
+        if jsonr == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(jsonr.get("id"), jsonr.get("name")))
+    except ValueError:
+        print("Not a valid JSON")

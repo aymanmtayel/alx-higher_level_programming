9b9e513e917ea413f8a37087cd027c1d5cef5f54
aymanmtayel <eng.aymantayel@gmail.com>
2024-04-26 19:03:58 +0300

    Task01 more neat

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """script that takes in a URL, and display the header of the response."""
 
-import sys
-import urllib.request
+from sys import argv
+from urllib import request
 
 
 if __name__ == "__main__":
-    URL = sys.argv[1]
-    with urllib.request.urlopen(URL) as response:
+    URL = argv[1]
+    with request.urlopen(URL) as response:
         VALUE = response.headers.get('X-Request-Id')
         print(VALUE)
